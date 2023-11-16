899d81552acdce5767ac25b176e8a6d7f14eca89
Cristian Aragón <crisarag8.sal@gmail.com>
2023-11-16 11:54:44 -0600

    Cambio de interfaz

--- src/main.py
+++ src/main.py
@@ -1,32 +1,32 @@
 import tkinter as tk
-from tkinter import ttk
 from tkinter import filedialog
+import shutil
+import os
 
-class Application(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("Compresor de Archivos")
-        self.geometry("700x600")
-        style = ttk.Style()
-        style.configure("TButton", padding=10, relief="flat", font=('Helvetica', 12))
-        style.configure("TLabel", font=('Helvetica', 12))
-        self.boton_buscar = ttk.Button(self, text="Buscar Archivo", command=self.buscar_archivo)
-        self.boton_buscar.pack(pady=20)
-        self.etiqueta_ruta = ttk.Label(self, text="")
-        self.etiqueta_ruta.pack()
-        self.boton2 = ttk.Button(self, text="Comprimir", command=self.accion_boton)
-        self.boton2.pack(pady=10)
-        self.boton3 = ttk.Button(self, text="Descomprimir", command=self.accion_boton)
-        self.boton3.pack(pady=10)
-        self.boton4 = ttk.Button(self, text="Cancelar", command=self.accion_boton)
-        self.boton4.pack(pady=10)
+class CompresorArchivoApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Compresor de Archivos")
+        self.root.geometry("300x200")
+        self.archivo_a_comprimir = tk.StringVar()
+        self.lbl_archivo = tk.Label(root, text="Selecciona el archivo a comprimir:")
+        self.lbl_archivo.pack(pady=10)
+        self.entry_archivo = tk.Entry(root, textvariable=self.archivo_a_comprimir, width=40)
+        self.entry_archivo.pack(pady=10)
+        self.btn_seleccionar = tk.Button(root, text="Seleccionar Archivo", command=self.seleccionar_archivo)
+        self.btn_seleccionar.pack(pady=10)
+        self.btn_comprimir = tk.Button(root, text="Comprimir Archivo", command=self.comprimir_archivo)
+        self.btn_comprimir.pack(pady=10)
 
-    def buscar_archivo(self):
-        self.ruta_archivo = filedialog.askopenfilename()
-        self.etiqueta_ruta.config(text=f"Archivo seleccionado: {self.ruta_archivo}")
+    def seleccionar_archivo(self):
+        archivo = filedialog.askopenfilename(initialdir="/", title="Seleccionar Archivo",
+                                            filetypes=(("Todos los archivos", "*.*"), ("Archivos de texto", "*.txt")))
+        self.archivo_a_comprimir.set(archivo)
 
-    def accion_boton(self):
-        print("Funcionando")
+    def comprimir_archivo(self):
+        pass
 
-app = Application()
-app.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = CompresorArchivoApp(root)
+    root.mainloop()
