63385fd6e1e82468c2c3082e09508b146b3b99b9
Cristian Aragón <crisarag8.sal@gmail.com>
2023-11-16 11:27:52 -0600

    Botón para buscar archivo

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 import tkinter as tk
+from tkinter import filedialog
 
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Compresor de Archivos")
         self.geometry("700x600")
+        self.boton_buscar = tk.Button(self, text="Buscar Archivo", command=self.buscar_archivo)
+        self.boton_buscar.pack()
+        self.etiqueta_ruta = tk.Label(self, text="")
+        self.etiqueta_ruta.pack()
+
+    def buscar_archivo(self):
+        self.ruta_archivo = filedialog.askopenfilename()
+        self.etiqueta_ruta.config(text=f"Archivo seleccionado: {self.ruta_archivo}")
 
 app = Application()
 app.mainloop()
