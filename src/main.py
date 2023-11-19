ad1402b2f0153a19cd3b2eaaa258afc93a24f2ee
Cristian Aragón <crisarag8.sal@gmail.com>
2023-11-18 22:23:45 -0600

    Mensajes de error o confirmación

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 import os
 import sys
 script_dir = os.getcwd()
@@ -30,6 +30,12 @@ class CompresorArchivoApp:
         self.btn_descomprimir = tk.Button(root, text="Descomprimir Archivo", command=self.descomprimir_archivo, state="disabled")
         self.btn_descomprimir.pack(side="right", padx=10)
 
+    def afirmative_message(self, message):
+        messagebox.showinfo("Éxito", message)
+
+    def error_message(self, error):
+        messagebox.showerror("Error", f"Se produjo un error: {error}")
+
     def seleccionar_archivo(self):
         self.archivo = filedialog.askopenfilename(initialdir="/", title="Seleccionar Archivo",
                                             filetypes=(("Todos los archivos", "*.*"), ("Archivos de texto", "*.txt")))
@@ -39,28 +45,34 @@ class CompresorArchivoApp:
             self.btn_descomprimir.config(state="normal")
 
     def comprimir_archivo(self):
-        file_name = os.path.splitext(os.path.basename(self.archivo))[0]
-        file_path = os.path.dirname(self.archivo)
-        output_file = os.path.join(file_path, f"{file_name}_compressed.txt")
-        tree_file = os.path.join(file_path, f"{file_name}_huffman_tree.txt")
-        self.huffman.process_text(self.archivo)
-        self.huffman.generate_huffman_codes(self.huffman.root)
-        with open(tree_file, 'wb') as tree_file:
-            self.huffman.serialize_huffman_tree(tree_file)
-        self.huffman.compress_file(self.archivo, output_file)
-        print("listo")
+        try:
+            file_name = os.path.splitext(os.path.basename(self.archivo))[0]
+            file_path = os.path.dirname(self.archivo)
+            output_file = os.path.join(file_path, f"{file_name}_compressed.txt")
+            tree_file = os.path.join(file_path, f"{file_name}_huffman_tree.txt")
+            self.huffman.process_text(self.archivo)
+            self.huffman.generate_huffman_codes(self.huffman.root)
+            with open(tree_file, 'wb') as tree_file:
+                self.huffman.serialize_huffman_tree(tree_file)
+            self.huffman.compress_file(self.archivo, output_file)
+            self.afirmative_message("Compresion exitosa")
+        except Exception as e:
+            self.error_message(e)
     
     def descomprimir_archivo(self):
-        file_name = os.path.splitext(os.path.basename(self.archivo))[0]
-        file_path = os.path.dirname(self.archivo)
-        output_file = os.path.join(file_path, f"{file_name}_decompressed.txt")
-        tree_file = os.path.join(file_path, f"{file_name}_huffman_tree.txt")
-        output_file = output_file.replace("_compressed", "")
-        tree_file = tree_file.replace("_compressed", "")
-        with open(tree_file, 'rb') as tree_file:
-            self.huffmanDecode.deserialize_huffman_tree(tree_file)
-        self.huffmanDecode.decompress_file(self.archivo, output_file)
-        print("listo")
+        try:
+            file_name = os.path.splitext(os.path.basename(self.archivo))[0]
+            file_path = os.path.dirname(self.archivo)
+            output_file = os.path.join(file_path, f"{file_name}_decompressed.txt")
+            tree_file = os.path.join(file_path, f"{file_name}_huffman_tree.txt")
+            output_file = output_file.replace("_compressed", "")
+            tree_file = tree_file.replace("_compressed", "")
+            with open(tree_file, 'rb') as tree_file:
+                self.huffmanDecode.deserialize_huffman_tree(tree_file)
+            self.huffmanDecode.decompress_file(self.archivo, output_file)
+            self.afirmative_message("Descompresion exitosa")
+        except Exception as e:
+            self.error_message(e)
 
 if __name__ == "__main__":
     root = tk.Tk()
